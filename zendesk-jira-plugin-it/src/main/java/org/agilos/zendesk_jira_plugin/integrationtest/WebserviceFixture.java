0fbc13a94ef6c480239e23eb7946ca63ca59f67d
mikis <mikis@f5b5b895-cfb1-4645-b906-73037d8a9abe>
2009-03-25 09:32:23 +0000

    ZEN-4: Setup integrationtest Fixed build after refactoring http://jira.agilos.org/browse/ZEN-4

---
+++ zendesk-jira-plugin-it/src/main/java/org/agilos/zendesk_jira_plugin/integrationtest/WebserviceFixture.java
@@ -0,0 +1,32 @@
+package org.agilos.zendesk_jira_plugin.integrationtest;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Calendar;
+import java.util.Date;
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
+import org.agilos.jira.soapclient.*;
+
+/**
+  * Sample JIRA SOAP client. Note that the constants sit in the {@link ClientConstants} interface
+ */
+public class WebserviceFixture implements ClientConstants
+{
+	private final JiraSoapService jiraSoapService;
+	private String token;
+	
+	public WebserviceFixture() throws ServiceException {
+		JiraSoapServiceService jiraSoapServiceGetter = new JiraSoapServiceServiceLocator();
+		jiraSoapService = jiraSoapServiceGetter.getJirasoapserviceV2();
+	}
+    
+	public void loginWithUserAndPassword(String login_name, String password) throws RemoteAuthenticationException, org.agilos.jira.soapclient.RemoteException, RemoteException {
+        token = jiraSoapService.login(login_name, password);
+	}
+	
+	public void createProject(String)
+}
