fce3a80558b3ad4efe21ad8dcc3dc8763798bd1c
mikis <mikis@f5b5b895-cfb1-4645-b906-73037d8a9abe>
2009-03-27 09:22:04 +0000

    ZEN-8 Setup integrationtest Finished

--- zendesk-jira-plugin-it/src/main/java/org/agilos/zendesk_jira_plugin/integrationtest/ClientConstants.java
+++ zendesk-jira-plugin-it/src/main/java/org/agilos/zendesk_jira_plugin/integrationtest/ClientConstants.java
@@ -5,34 +5,16 @@ import java.util.Date;
 interface ClientConstants
 {
     // Login details
-    static final String LOGIN_NAME = "soaptester";
-    static final String LOGIN_PASSWORD = "soaptester";
+    static final String LOGIN_NAME = "bamboo";
+    static final String LOGIN_PASSWORD = "bamboo2997";
 
-    // Constants for issue creation
     static final String PROJECT_KEY = "TST";
+    static final String PROJECT_NAME = "Dee Project";
+    static final String PROJECT_DESCRIPTION = "This is a project created by soap on: " + new Date();
     static final String ISSUE_TYPE_ID = "1";
     static final String SUMMARY_NAME = "This is a new SOAP issue " + new Date();
     static final String PRIORITY_ID = "4";
     static final String COMPONENT_ID = "10240";
     static final String VERSION_ID = "10330";
-
-    // Constants for issue update
-    static final String NEW_SUMMARY = "New summary";
-    static final String CUSTOM_FIELD_KEY_1 = "customfield_10061";
-    static final String CUSTOM_FIELD_VALUE_1 = "10098";
-    static final String CUSTOM_FIELD_KEY_2 = "customfield_10061:1";
-    static final String CUSTOM_FIELD_VALUE_2 = "10105";
-
-    // Constant for add comment
     static final String NEW_COMMENT_BODY = "This is a new comment";
-
-    // Constant for get filter
-    static final String FILTER_ID = "12355";
-    static final String SEARCH_TERM = "remote";
-
-    // Constants for project creation
-    static final String CREATE_PROJECT_KEY = "DUD";
-    static final String PROJECT_NAME = "Dee Project";
-    static final String PROJECT_DESCRIPTION = "This is a project created by soap on: " + new Date();
-
 }
--- zendesk-jira-plugin-it/src/main/java/org/agilos/zendesk_jira_plugin/integrationtest/WebserviceFixture.java
+++ zendesk-jira-plugin-it/src/main/java/org/agilos/zendesk_jira_plugin/integrationtest/WebserviceFixture.java
@@ -1,34 +1,63 @@
 package org.agilos.zendesk_jira_plugin.integrationtest;
 
-import java.io.*;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Calendar;
-import java.util.Date;
 import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.Set;
 
 import javax.xml.rpc.ServiceException;
 
 import org.agilos.jira.soapclient.*;
+import org.apache.commons.lang.NotImplementedException;
 
-/**
-  * Sample JIRA SOAP client. Note that the constants sit in the {@link ClientConstants} interface
- */
-public class WebserviceFixture implements ClientConstants
-{
+public class WebserviceFixture {
 	private final JiraSoapService jiraSoapService;
 	private String token;
-	
-	public WebserviceFixture() throws ServiceException {
+	private RemoteProject project;
+
+	static final String LOGIN_NAME = "bamboo";
+	static final String LOGIN_PASSWORD = "bamboo2997";
+
+	static final String JIRA_URL = "http://192.168.0.100:8080";
+	static final String PROJECT_KEY = "TST";
+	static final String PROJECT_NAME = "Test Project";
+	static final String PROJECT_DESCRIPTION = "This is a Zendesk JIRA plugin integrationtest project " + new Date();
+	static final String PROJECT_LEAD = "bamboo";
+
+	public WebserviceFixture() throws ServiceException,
+			RemoteAuthenticationException,
+			org.agilos.jira.soapclient.RemoteException, RemoteException {
 		JiraSoapServiceService jiraSoapServiceGetter = new JiraSoapServiceServiceLocator();
 		jiraSoapService = jiraSoapServiceGetter.getJirasoapserviceV2();
+		token = jiraSoapService.login(LOGIN_NAME, LOGIN_PASSWORD);
 	}
-    
-	public void loginWithUserAndPassword(String login_name, String password) throws RemoteAuthenticationException, org.agilos.jira.soapclient.RemoteException, RemoteException {
-        token = jiraSoapService.login(login_name, password);
+
+	public void createProject() throws Exception {
+		project = jiraSoapService.createProject(token, PROJECT_KEY, PROJECT_NAME, PROJECT_DESCRIPTION, null, PROJECT_LEAD, new RemotePermissionScheme(null, new Long(0), null, null, null), null, null);	
 	}
-	
-	public void createProject() {
-		
+
+	public void removeProject() throws Exception {
+		jiraSoapService.deleteProject(token, PROJECT_KEY);
+	}
+
+	public void addUser(String name) throws Exception  {
+		jiraSoapService.createUser(token, name, name+"-pw", "Test User "+name, name+"@nowhere.test");
+	}
+
+	public void removeUser(String name) throws Exception  {
+		jiraSoapService.deleteUser(token, name);
+	}
+
+	public void assignUserToProject(String name) throws Exception {
+		RemoteProjectRole developerRole = jiraSoapService.getProjectRoles(token)[1];
+		jiraSoapService.getProjectRoleActors(token, developerRole, project); 
+		jiraSoapService.addActorsToProjectRole(token, new String[] { name }, new RemoteProjectRole(null, new Long(83), null), project, "atlassian-user-role-actor"); 
+	}
+
+	public void unassignUserFromProject(String name) throws Exception  {
+		throw new NotImplementedException();
+	}
+
+	public Set getAssignableUsers() throws Exception {
+		throw new NotImplementedException();
 	}
 }
