d45ce4ba00b52d7eb05ee3a21e2a5338b99d602a
mikis <mikis@f5b5b895-cfb1-4645-b906-73037d8a9abe>
2009-04-03 07:00:14 +0000

    ZEN-4: Setup integrationtest, ZEN-9 Automatic plugin deployment Finished

--- src/main/java/org/agilos/jira/service/UserServiceImpl.java
+++ src/main/java/org/agilos/jira/service/UserServiceImpl.java
@@ -1,12 +1,23 @@
 package org.agilos.jira.service;
 
-import org.apache.commons.lang.NotImplementedException;
+import org.ofbiz.core.entity.GenericValue;
 
+import com.atlassian.jira.ManagerFactory;
 import com.atlassian.jira.notification.type.RemoteUser;
+import com.atlassian.jira.permission.PermissionContext;
+import com.atlassian.jira.permission.PermissionContextFactory;
+import com.atlassian.jira.security.Permissions;
 
 public class UserServiceImpl implements UserService {
-
+	private final PermissionContextFactory permissionContextFactory;
+	
+	public UserServiceImpl(PermissionContextFactory permissionContextFactory) {
+		this.permissionContextFactory = permissionContextFactory;
+	}
+	
 	public RemoteUser[] getAssignableUsers(String projectKey) {
-		throw new NotImplementedException();
+		GenericValue project = ManagerFactory.getProjectManager().getProjectByKey(projectKey);
+		PermissionContext ctx = permissionContextFactory.getPermissionContext(project);
+		return (RemoteUser[])ManagerFactory.getPermissionSchemeManager().getUsers(new Long(Permissions.ASSIGNABLE_USER), ctx).toArray(new RemoteUser[0]);
 	}
 }
