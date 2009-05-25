41f6b06e7fe05cdd0b79f551f6658220e090e7ee
mikis <mikis@f5b5b895-cfb1-4645-b906-73037d8a9abe>
2009-05-25 08:29:51 +0000

    Tilføjet curl WS test scripts.

---
+++ src/test/scripts/getAssignableUser.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -u bamboo:bamboo2997 -H "SOAPAction: foo" -H "Content-Type: text/xml;charset=UTF-8" -d @messages/getAssignableUsers.xml -X POST http://localhost:1990/jira/rpc/soap/agilossoapservice-v1
