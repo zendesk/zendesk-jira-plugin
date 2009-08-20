a76ae4fdf837fbba8e03a07969f7f0cc8bcbf564
Mikis Seth Sorensen <mikis@agilis-software.dk>
2009-08-20 12:31:45 +0200

    ZEN-29 Integrationtest maintaince, changed serverside dedployment script

--- src/test/java/it/org/agilos/zendesk_jira_plugin/integrationtest/JIRATest.java
+++ src/test/java/it/org/agilos/zendesk_jira_plugin/integrationtest/JIRATest.java
@@ -3,13 +3,12 @@ package it.org.agilos.zendesk_jira_plugin.integrationtest;
 import java.util.Date;
 
 import org.agilos.zendesk_jira_plugin.integrationtest.fixtures.JIRAFixture;
-import org.testng.annotations.AfterMethod;
 
 public abstract class JIRATest {
 
-	protected static final String JIRA_URL = "http://192.168.0.100:8080";
-	protected static final String LOGIN_NAME = "bamboo";
-	protected static final String LOGIN_PASSWORD = "bamboo2997";
+//	protected static final String JIRA_URL = "http://192.168.0.100:8080";
+//	protected static final String LOGIN_NAME = "bamboo";
+//	protected static final String LOGIN_PASSWORD = "bamboo2997";
 	
     protected static final String PROJECT_KEY = "WST";
     protected static final String USER_ID = "integration";
